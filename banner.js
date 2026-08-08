baeb94fc2b1f4894ec9260fd78c371a2bfc9e0e7
C1FR4 <salazarrodriguezadriano41@gmail.com>
2026-08-07 19:40:16 -0500

    refactor: hardcodear spider art en banner.js y eliminar spider.txt
    - Embeber SPIDER_ART como constante en banner.js (arte estatico, sin indireccion de archivo)
    - Eliminar dependencia fs/path y riesgo de crash si falta spider.txt
    - Fusionar duplicados de colorize (ambos devolvian CYAN)
    - Eliminar spider.txt del repo

--- banner.js
+++ banner.js
@@ -1,9 +1,6 @@
 // banner.js
 // Imprime la araña ASCII + un menú/caja centrados y armonizados,
-// con colores ANSI.
-
-const fs = require("fs");
-const path = require("path");
+// con colores ANSI. El arte está embebido: si lo cambias, edita SPIDER_ART.
 
 const RESET = "\x1b[0m";
 const DIM = "\x1b[2m";
@@ -13,23 +10,58 @@ const MAGENTA = "\x1b[35m";
 const WHITE = "\x1b[97m";
 const GRAY = "\x1b[90m";
 
+const SPIDER_ART = `  
+             ,,                                                     ,,
+             #,..                                                #..,
+                 :--                                           --:@
+                 . ==+                                       +==   :
+                      +*$                                 $*+
+                        $$$         .         .         $$$
+                         -$$        ..       ..        $$
+                          # &&       *       *       &&
+         ,,..              %  &       *     *       &                 ..,,
+            :::-==++           BB     $ % % $     BB.          ++==-:::
+                   **$$$         88  .&%%#MW&   88         $$$**
+                        $$&&&     88:%%@@@@@8B 88     &&&$$
+                             BBB    %%@@@%#M8B$    BBB
+                                888 #%%@@@%M8&* 888
+                                   WM##%@@#WB$+-
+                                 88 WWMM#MW8&*=@88
+                               B88  BB88:-:$+=: :88B
+                            BBB   88 $  :-:  . 88   BBB
+                      %@  &&&    88     :-:     88    &&&   %
+                     - $$$      B8      :-:      8B      $$$ :
+                     $$$       BB       :-:       BB       $$$
+                  ++*$        &      %  :--  W      &        $*++
+                ==          &&     %%%%%,--#MWW8     &&       %  ==
+             ::--          $&    %%%%%%%%%##MMW88B    &$          --::
+           ..             $$    :%%@@@@%,%%##MW88B&    $$             ..
+         ,,             $$$    %%:%@@@@@@,%#,M:88B&$    $$$             ,,
+                       *$     ##%%-%@@@:-@%%#MW8BB&$*     $*
+                      +*    - ,##%,%@@,,-@%:,M..B&$*+      *+
+                     =       #M,#.%%%,.@@@@.#MW8B&$*+        =
+                   --        WMM.##.%%%@@@%#MW8B&$*+=-        --
+                  :           WW:MM,#-%%%%#:.W-.&-+.-           :
+                ..            88WWWMMMMM,,M,8BB:$*+=-            ..
+               ,.             BB888,WWWWWW88:-$-:+=-:             .,
+              ,,               &BBBBBBB:BB-&$$*.=-::               ,,
+                                $&&:&&&&&$$*:,+=-:.@
+                                 **,***:**++==-::.  .
+                                   +++++===--::.
+                                     ----:::..`;
+
 const stripAnsi = (s) => s.replace(/\x1b\[[0-9;]*m/g, "");
 const visibleLength = (s) => stripAnsi(s).length;
 
 function colorize(char) {
-  if ("@%#".includes(char)) return CYAN + char + RESET;
-  if ("MW8B".includes(char)) return CYAN + char + RESET;
+  if ("@%#MW8B".includes(char)) return CYAN + char + RESET;
   if ("&$*+".includes(char)) return DIM + CYAN + char + RESET;
   if ("=-:.,".includes(char)) return GRAY + char + RESET;
   return char;
 }
 
 function printSpider() {
-  const art = fs
-    .readFileSync(path.join(__dirname, "spider.txt"), "utf8")
-    .split("\n")
-    .filter((l, i, arr) => !(i === arr.length - 1 && l === ""));
-
+  const art = SPIDER_ART.split("\n");
   let artWidth = 0;
   for (const line of art) {
     artWidth = Math.max(artWidth, line.length);
